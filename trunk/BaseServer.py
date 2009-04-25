8e9b6bcf5739995125c814302524c0fb43fedd54
doggod999 <doggod999@5699706e-2a9f-11de-9f0d-3bffa153b7de>
2009-04-25 05:54:48 +0000

    基本完成 get方法可以返回网页 相对路径有错

--- trunk/BaseServer.py
+++ trunk/BaseServer.py
@@ -6,7 +6,7 @@ import socket
 
 import config
 import log
-import HttpRequest
+import HttpRequest, HttpResponse
 
 class BaseServer():
     def __init__(self):
@@ -18,29 +18,42 @@ class BaseServer():
         self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.socket.bind((config.HOST, config.PORT))
         self.socket.listen(5)
-        self.logger.info('Server start up successfully on port %d...\n' % config.PORT)
+        self.logger.info('Server start up successfully on port %d...\r\n' % config.PORT)
         while True:
             self.handle_request()
         
     def handle_request(self):
         connect, client_addr = self.socket.accept()
+        self.http_response = HttpResponse.HttpResponse(connect)
+        
         client_ip = client_addr[0]
         client_name = socket.getfqdn(client_ip)
-        print client_ip
-        print client_name
         
         request = connect.recv(1024)
         self.http_request = HttpRequest.HttpRequest(request)
         
-        msg = '[%s] -- %s\n' % (client_name, self.http_request.request_dict['request_line'])
-        self.logger.info(msg)
+        self.log_msg = '[%s] - - "%s"' % (client_name, self.http_request.request_dict['request_line'])
         
-        connect.send('welcome')
-        self.doGET()
+        if self.http_request.request_dict['method'] == 'GET':
+            self.doGET()
+        else:
+            self.http_response.send_error(400, self.http_request.request_dict['method'])
+            self.log_msg += ' 400\r\n'
+            self.logger.info(self.log_msg)
         connect.close()
         
     def doGET(self):
-        pass
+        file = self.http_response.get_file(self.http_request.request_dict['url'], config.apppath)
+        if file is None:
+            self.http_response.send_error(404, self.http_request.request_dict['url'])
+            self.log_msg += ' 404\r\n'
+            self.logger.info(self.log_msg)
+        else:
+            f = open(file, 'rb')
+            body = f.read()
+            self.http_response.send_response(200, body)
+            self.log_msg += ' 200\r\n'
+            self.logger.info(self.log_msg)
 
 if __name__ == '__main__':
     BaseServer()
